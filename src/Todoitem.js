4cda35acccb5de2ddaecfeb18c5c13adbf3e3431
tefrancisco <dzfranciscodz@gmail.com>
2025-03-30 14:21:23 -0300

    Applying state method to change the color onlick

--- src/Todoitem.js
+++ src/Todoitem.js
@@ -4,13 +4,30 @@ import "./App.css";
 // We are using XML, not HTML, pay attention
 
 export default class Todoitem extends React.Component {
+
+  // Once props are imutable, we use state to track and change data
+
+  constructor(props) {
+    super(props);
+
+    // Now completed is a state variable, so it's mutable
+    this.state = {
+      completed: props.completed
+    }
+  }
+
+
   render() {
     // Simplified destructuring of the props object
-    let {title, description, completed} = this.props;
+    let {title, description} = this.props;
+    let completed = this.state.completed;
     // Designando as condições para as variáveis e melhorando a leitura do código
+    // Props são imutáveis
     let descriptionText = (!description) ? "default text" : description
     let completedText = (completed) ? "Completed" : "Not Completed"
     let completedClass = (completed) ? "btn btn-success" : "btn btn-danger"
+
+    console.log('render')
 
     return (
 
@@ -26,7 +43,11 @@ export default class Todoitem extends React.Component {
           </p>
         </div>
         
-        <button className={completedClass}>{completedText}</button>
+        <button 
+        className={completedClass}
+        // Using the method of setState to change the state of the component
+        onClick={() => this.setState({completed: !completed})}
+        >{completedText}</button>
       </div>
     );
   }
